5f157baf6125880380af72afa6774c4f83591620
lamngoc1234v-pixel <lamngoc1234v@gmail.com>
2026-04-21 15:25:01 +0700

    Update analytics.js

--- api/analytics.js
+++ api/analytics.js
@@ -1,27 +1,14 @@
 export default async function handler(req, res) {
-
     const { type } = req.query;
-
     const token = process.env.TELEGRAM_TOKEN;
-
     const chatId = process.env.CHAT_ID;
-
-
 
     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent("Bí mật: Người ấy vừa chọn " + type)}`;
 
-
-
     try {
-
         await fetch(url);
-
         res.status(200).json({ status: "xong" });
-
     } catch (e) {
-
         res.status(500).json({ status: "lỗi" });
-
     }
-
-} 
+}
