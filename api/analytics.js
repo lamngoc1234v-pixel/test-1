c425538c34a0fc2ec35a95c7d1e1002d84b6735f
lamngoc1234v-pixel <lamngoc1234v@gmail.com>
2026-04-21 15:22:03 +0700

    Update analytics.js

--- api/analytics.js
+++ api/analytics.js
@@ -11,4 +11,4 @@ export default async function handler(req, res) {
     } catch (e) {
         res.status(500).json({ status: "lỗi" });
     }
-} sửa, code để trong api không bị f12 ra đúng không 
+} 
